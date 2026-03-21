b95268d2caac3437f3e54202c55d1ad92e98fb8f
dunguyen2221 <dunguyen20112000@gmail.com>
2026-03-21 09:50:03 +0700

    lô cu

---
+++ app/src/main/java/com/example/yuxiaofy/MusicService.kt
@@ -0,0 +1,27 @@
+package com.example.yuxiaofy
+
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaSession
+import androidx.media3.session.MediaSessionService
+
+class MusicService : MediaSessionService() {
+    private var mediaSession: MediaSession? = null
+    private lateinit var player: ExoPlayer
+
+    override fun onCreate() {
+        super.onCreate()
+        player = ExoPlayer.Builder(this).build()
+        mediaSession = MediaSession.Builder(this, player).build()
+    }
+
+    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? = mediaSession
+
+    override fun onDestroy() {
+        mediaSession?.run {
+            player.release()
+            release()
+            mediaSession = null
+        }
+        super.onDestroy()
+    }
+}
